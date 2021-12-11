685bc40024b595f11082095b311627619211e8d8
Daniil Tsybulnikau <83131236+dan-tsybulnikau@users.noreply.github.com>
2021-12-11 18:19:40 +0300

    Refactor after evaluation
    1) Argparse module implemented
    2) Decreased complexity of base sorting logic (from O(n*m {iterating all students whithin iterating all rooms} to O(n+m {iterating all students, and then iterating all rooms})
    3) Factory for output format created
    4) Class refactoring done - classes to read, sort, format, write

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import json
+import pprint
+import argparse
+import sys
 
 XML_SKELETON = """<?xml version="1.0" encoding="utf-8" ?>
 <root>
@@ -19,10 +22,30 @@ STUDENTS_SKELETON = """
 """
 
 
-class DataDriver:
-    """Base class for data manipulations"""
+class CLIParser:
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+
+    def parse_args(self):
+        self.parser.add_argument('source_to_rooms_file', type=str, action='store', nargs='?')
+        self.parser.add_argument('source_to_students_file', type=str, action='store', nargs='?')
+        self.parser.add_argument('output_format', type=str, action='store', nargs='?')
+        args = self.parser.parse_args()
+        if None in vars(args).values():
+            raise SystemExit("Error: all arguments must be given")
+        return args
+
+
+class DataReader:
     def __init__(self):
         self.data = None
+
+    def read_data(self, path: str):
+        raise NotImplementedError
+
+
+class JsonReader(DataReader):
 
     def __convert_data(self):
         return json.loads(self.data)
@@ -32,37 +55,44 @@ class DataDriver:
             self.data = data.read()
         return self.__convert_data()
 
-    def write_data(self, data):
+
+class DataSorter:
+    def __init__(self, rooms_data, students_data):
+        self.sorted_students = {}
+        self.rooms = rooms_data
+        self.students = students_data
+
+    def sort_data(self):
         raise NotImplementedError
 
 
-class JSONWriter(DataDriver):
-    def write_data(self, data):
-        with open('result.json', mode='w') as result_file:
-            result_file.write(data)
-
-
-class XMLWriter(DataDriver):
-    def write_data(self, data):
-        with open('result.xml', mode='w') as result_file:
-            result_file.write(data)
+class StudentInRoomSorter(DataSorter):
+    def sort_data(self):
+        for person in self.students:
+            student_room_number = person.get('room')
+            self.sorted_students[student_room_number] = self.sorted_students.get(student_room_number, []) + [person]
+        for room in self.rooms:
+            room['students'] = self.sorted_students.get(room['id'], [])
+        return self.rooms
 
 
 class DataFormatter:
+    def __init__(self, data):
+        self.data = data
 
-    @staticmethod
-    def format_student_data(data: dict):
-        del data['room']
-        return data
+    def format_data(self):
+        raise NotImplementedError
 
-    @staticmethod
-    def format_to_json(data: list):
-        return json.dumps(data)
 
-    @staticmethod
-    def format_to_xml(data: list):
+class DataToJsonFormatter(DataFormatter):
+    def format_data(self):
+        return json.dumps(self.data)
+
+
+class DataToXmlFormatter(DataFormatter):
+    def format_data(self):
         xml_text = ''
-        for row in data:
+        for row in self.data:
             student_info = ''
             room_id = row.get("id")
             room_name = row.get("name")
@@ -72,26 +102,45 @@ class DataFormatter:
         return XML_SKELETON % xml_text
 
 
+class DataWriter:
+    def __init__(self):
+        self.formats = {'json': DataToJsonFormatter,
+                        'xml': DataToXmlFormatter,
+                        }
+
+    def write_data(self, data: list, output_format: str):
+        try:
+            driver = self._select_driver(output_format)
+        except ValueError:
+            print("Not supported output format")
+            exit()
+        else:
+            output_data = driver(data).format_data()
+            with open(f'result.{output_format}', mode='w') as result_data:
+                result_data.write(output_data)
+
+    def _select_driver(self, output_format: str):
+        if output_format not in self.formats:
+            raise ValueError('Not')
+        else:
+            return self.formats.get(output_format)
+
+
+def main():
+    cli_parser = CLIParser()
+    options = cli_parser.parse_args()
+
+    data_reader = JsonReader()
+    rooms = data_reader.read_data(options.source_to_rooms_file)
+    students = data_reader.read_data(options.source_to_students_file)
+    result_file_format = options.output_format.lower()
+
+    rooms_with_students_sorter = StudentInRoomSorter(rooms_data=rooms, students_data=students)
+    rooms_with_students = rooms_with_students_sorter.sort_data()
+
+    output_data_writer = DataWriter()
+    output_data_writer.write_data(data=rooms_with_students, output_format=result_file_format)
+
+
 if __name__ == '__main__':
-    path_to_rooms_file = input('Enter path to rooms file (absolute): ')
-    path_to_students_file = input('Enter path to students file (absolute): ')
-    output_format = input('Enter path to rooms file (JSON or XML): ')
-    if output_format not in ['JSON', 'XML']:
-        print('Not valid output format.')
-        exit()
-    data_driver = DataDriver()
-    rooms = data_driver.read_data(path_to_rooms_file)
-    students = data_driver.read_data(path_to_students_file)
-    for room in rooms:
-        room['students'] = [DataFormatter.format_student_data(student) for student in students
-                            if student.get('room', None) == room['id']]
-
-    if output_format == 'JSON':
-        writer = JSONWriter()
-        output_data = DataFormatter.format_to_json(rooms)
-    elif output_format == 'XML':
-        writer = XMLWriter()
-        output_data = DataFormatter.format_to_xml(rooms)
-
-    writer.write_data(output_data)
-
+    main()
